bef57dda0572a505888a472bff9a1169182c03ea
kjcaway <whdckstngh@naver.com>
2021-01-10 23:28:22 +0900

    ** ADD - CommonFilter

---
+++ src/main/kotlin/me/examplewebmvc/filter/CommonFilter.kt
@@ -0,0 +1,20 @@
+package me.examplewebmvc.filter
+
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.GenericFilterBean
+import javax.servlet.FilterChain
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
+import javax.servlet.http.HttpServletRequest
+
+@Component
+@Order(1)
+class CommonFilter: GenericFilterBean() {
+    override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
+        val httpServletRequest = request as HttpServletRequest
+        logger.info("Reqeust method : ${httpServletRequest.method}, url : ${httpServletRequest.requestURL}")
+        chain?.doFilter(request, response)
+        logger.info("Response content-type : ${response?.contentType}")
+    }
+}
