687824f66d24b60a944bbe7b9905673fdf8e1804
kjcaway <whdckstngh@naver.com>
2021-01-27 22:04:43 +0900

    **MOD -uri pattern filter

--- src/main/kotlin/me/examplewebmvc/filter/CommonFilter.kt
+++ src/main/kotlin/me/examplewebmvc/filter/CommonFilter.kt
@@ -1,5 +1,6 @@
 package me.examplewebmvc.filter
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.GenericFilterBean
@@ -11,14 +12,22 @@ import javax.servlet.http.HttpServletRequest
 @Component
 @Order(1)
 class CommonFilter: GenericFilterBean() {
+    @Value("\${api.base.path:/api}")
+    private val apiBasePath: String = ""
+
     override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
         val httpServletRequest = request as HttpServletRequest
-        logger.info("Reqeust method : ${httpServletRequest.method}, url : ${httpServletRequest.requestURL}")
-        try {
-            chain?.doFilter(request, response)
-            logger.info("Response content-type : ${response?.contentType}")
-        } catch (e: Exception){
-            logger.error(e.printStackTrace())
+        if(includeUrlPattern(httpServletRequest)){
+            logger.info("Reqeust method : ${httpServletRequest.method}, url : ${httpServletRequest.requestURL}")
         }
+        chain?.doFilter(request, response)
+    }
+
+    private fun includeUrlPattern(httpServletRequest: HttpServletRequest): Boolean{
+        val uri = httpServletRequest.requestURI
+        if(uri.startsWith(apiBasePath)){
+            return true
+        }
+        return false
     }
 }
