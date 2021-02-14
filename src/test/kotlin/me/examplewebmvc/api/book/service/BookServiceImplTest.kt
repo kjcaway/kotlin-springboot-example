5da631c3ec7e1c32c3be01ab0f9e3a0990945bea
kjcaway <whdckstngh@naver.com>
2021-02-14 16:22:00 +0900

    **REFACTOR -test code @ActiveProfiles

--- src/test/kotlin/me/examplewebmvc/api/book/service/BookServiceImplTest.kt
+++ src/test/kotlin/me/examplewebmvc/api/book/service/BookServiceImplTest.kt
@@ -8,8 +8,10 @@ import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("local")
 class BookServiceImplTest {
     @Autowired
     private lateinit var bookService: BookService
