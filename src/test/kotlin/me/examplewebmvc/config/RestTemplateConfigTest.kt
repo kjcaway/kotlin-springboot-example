a61a4931a9e09d6ef2781e699bd2ad9d7ded502f
kjcaway <whdckstngh@naver.com>
2021-02-26 00:00:40 +0900

    ** REFACTOR -line cleanup

--- src/test/kotlin/me/examplewebmvc/config/RestTemplateConfigTest.kt
+++ src/test/kotlin/me/examplewebmvc/config/RestTemplateConfigTest.kt
@@ -1,16 +1,12 @@
 package me.examplewebmvc.config
 
-import com.fasterxml.jackson.core.type.TypeReference
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.ResponseEntity
-import org.springframework.test.context.ActiveProfiles
-import org.springframework.web.client.RestTemplate
-import java.util.HashMap
-
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.HttpMethod
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.web.client.RestTemplate
 
 
 @SpringBootTest
