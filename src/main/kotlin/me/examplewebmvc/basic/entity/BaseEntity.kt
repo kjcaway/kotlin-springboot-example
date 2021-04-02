c6b111bb9374d17d81b60a3de9d18e81a370a528
kjcaway <whdckstngh@naver.com>
2021-04-02 22:14:45 +0900

    **REFACTOR -updatable false

--- src/main/kotlin/me/examplewebmvc/basic/entity/BaseEntity.kt
+++ src/main/kotlin/me/examplewebmvc/basic/entity/BaseEntity.kt
@@ -2,12 +2,14 @@ package me.examplewebmvc.basic.entity
 
 import me.examplewebmvc.util.DateUtil
 import java.sql.Timestamp
+import javax.persistence.Column
 import javax.persistence.MappedSuperclass
 import javax.persistence.PrePersist
 import javax.persistence.PreUpdate
 
 @MappedSuperclass
 abstract class BaseEntity {
+    @Column(updatable = false)
     lateinit var createdTime: Timestamp
     lateinit var modifyTime: Timestamp
 
